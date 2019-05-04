05d5ff1017098af8d627655eb0bcff6833bd4fa8
nicolemibarra <nicolemibarra@icloud.com>
2019-05-04 13:31:08 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+//Node
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//Port
+var PORT = process.env.PORT || 8000;
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var router = require('./controllers/burgers_controller.js');
+app.use('/', router);
+
+app.listen(PORT, function() 
+{
+  console.log("App listening on PORT " + PORT);
+});
